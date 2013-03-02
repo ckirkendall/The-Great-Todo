34847d6280e9451c6204abb0d2f4c62b2acf55dc
ckirkendall <ckirkendall@gmail.com>
2013-03-02 10:59:05 -0500

    updated to enfocus 1.0.0

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject cinjug1 "1.0.0-SNAPSHOT"
   :description "best todo app ever"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.2.0"]
-                 [fetch "0.1.0-alpha2"]
-                 [enfocus "1.0.0-alpha2"]
-                 [com.novemberain/monger "1.0.0-beta4"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0"]
+                 [fetch "0.1.0-alpha2" :exclusions [org.clojure/clojure]]
+                 [enfocus "1.0.0"]
+                 [com.novemberain/monger "1.4.2"]
                  [clj-crypto "1.0.0"]]
+  :plugins      [[lein-cljsbuild "0.2.10"]]
   :main cinjug1.core
   :cljsbuild {
       :builds [{
